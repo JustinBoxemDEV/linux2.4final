76440bcface8969f72ad79f794528f2529c81ddf
Justin Boxem <justinboxem@hotmail.com>
2020-06-30 18:25:48 +0200

    Update auto-minion.sh

--- auto-minion.sh
+++ auto-minion.sh
@@ -1,10 +1,10 @@
-#! /bin/bash
+#!/bin/bash
   
 #installing salt-minion service
 curl -L https://bootstrap.saltstack.com -o install_salt.sh
 sudo sh install_salt.sh
 
-if ! grep -Fxg "master: 10.0.6.73" "etc/salt/minion";
+if ! grep -Fxq "master: 10.0.6.73" "etc/salt/minion";
 then
   echo "master: 10.0.6.73" >> etc/salt/minion
   sudo systemctl restart salt-minion
